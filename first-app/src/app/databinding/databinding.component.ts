bc777f0482724e149984cc786e5d06c6091bb7d7
scottyvg <svangilder@gmail.com>
2016-11-12 22:28:40 -0700

    event binding

--- first-app/src/app/databinding/databinding.component.ts
+++ first-app/src/app/databinding/databinding.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { PropertyBindingComponent } from './property-binding.component'
+import { EventBindingComponent } from './event-binding.component'
 
 @Component({
   selector: 'fa-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
+  directives: [PropertyBindingComponent, EventBindingComponent]
 })
 export class DatabindingComponent implements OnInit {
   stringInterpolation = 'This is string interpolation!';
   numberInterpolation = 2;
+
+  onTest() {
+    return true;
+  }
 }
